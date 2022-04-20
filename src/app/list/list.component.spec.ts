ccdc55c7843fc49437a4c57110f2d0b85fa21b3e
jm <biaggi>
2022-04-20 18:03:17 +0200

    chore(): basic app test

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -2,24 +2,45 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
 
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { initialState, AppState } from '../store/store';
+import { employeeMock } from '../model/employee.mocks';
+import { By } from '@angular/platform-browser';
+
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  const initialStateTest: AppState = {
+    employees: { employees: [employeeMock] },
+  };
+
+  let store: MockStore<AppState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [ListComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
-  beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
+    store.setState(initialStateTest);
+
+    spyOn(store, 'dispatch').and.callFake(() => {});
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should see a employee', () => {
+    console.log('a', fixture.debugElement.queryAll(By.css('.employee')));
+    expect(fixture.debugElement.queryAll(By.css('.employee')).length).toBe(1);
+  });
+
+
+
 });
